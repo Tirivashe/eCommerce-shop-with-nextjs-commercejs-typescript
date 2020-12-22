73933676d48700307333f6ebe63a1a856fb3b5c0
Tiri <shaymusts@gmail.com>
2020-12-22 08:59:22 -0600

    updated error handler

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -9,7 +9,7 @@ const Cart: FC = () => {
   const { cart } = useStoreContext()
   const classes = useStyles()
 
-  return cart !== undefined ||cart.line_items !== undefined ? (
+  return cart.line_items !== undefined ? (
     
     <Container className={classes.container}>
       <div className={classes.toolbar}>
