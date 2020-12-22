933a7c64d6a741a14051b39abff5b1f633174476
Tiri <shaymusts@gmail.com>
2020-12-21 22:17:27 -0600

    updated

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -9,7 +9,7 @@ const Cart: FC = () => {
   const { cart } = useStoreContext()
   const classes = useStyles()
 
-  return cart !== undefined ? (
+  return cart !== undefined ||cart.line_item !== undefined ? (
     
     <Container className={classes.container}>
       <div className={classes.toolbar}>
